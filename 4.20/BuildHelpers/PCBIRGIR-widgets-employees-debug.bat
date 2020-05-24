93713f6aa8b80461f7f6082a00c3eb7e8311ef06
Birgir Kristmannsson <birgir@centara.com>
2020-05-24 10:40:32 +0000

    Minor build changes

---
+++ 4.20/BuildHelpers/PCBIRGIR-widgets-employees-debug.bat
@@ -0,0 +1,17 @@
+@echo off
+set plugin=Widgets.Employees
+echo Running custom build step for %plugin%
+set nopOut="n:\nopCommerce 4.20\Presentation\Nop.Web\Plugins\%plugin%"
+if not exist %nopOut% (
+    mkdir %nopOut%
+    echo Created %nopOut%
+)
+if not exist %nopOut%\Views (
+    mkdir %nopOut%\Views
+    echo Created %nopOut%Views
+)
+xcopy /d /y /Q "..\_build\debug\logo.jpg" %nopOut%\. > nul
+xcopy /d /y /Q "..\_build\debug\plugin.json" %nopOut%\. > nul
+xcopy /d /y /Q "..\_build\debug\Nop.Plugin.%plugin%*.*" %nopOut%\. > nul
+xcopy /d /y /Q "..\_build\debug\Views\." %nopOut%\Views\. > nul
+echo %nopOut% was updated
