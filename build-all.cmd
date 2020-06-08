48852c07b6d79f64c9325c5b63df959e7ed76cb4
Birgir Kristmannsson <birgir@centara.com>
2020-06-08 21:39:56 +0000

    Build 4.2 and 4.3

--- build-all.cmd
+++ build-all.cmd
@@ -1,11 +1,11 @@
 @echo off
 set sln_file=Nop.Plugin.Widgets.Employees
 
-cd 4.20
+cd 4.30
 dotnet build %sln_file% --configuration=Release
 dotnet build %sln_file% --configuration=Debug
 
-cd ..\4.10
+cd ..\4.20
 dotnet build %sln_file% --configuration=Release
 dotnet build %sln_file% --configuration=Debug
 
