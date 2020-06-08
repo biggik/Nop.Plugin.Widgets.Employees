f86e41344948b12e5b479c27b977d283fa5e934a
Birgir Kristmannsson <birgir@centara.com>
2020-06-08 21:46:23 +0000

    Manage resources on uninstall

--- Common/EmployeesPlugin.cs
+++ Common/EmployeesPlugin.cs
@@ -11,22 +11,26 @@ using Nop.Core;
 using Nop.Services.Plugins;
 using Nop.Plugin.Widgets.Employees.Resources;
 using Nop.Web.Framework.Menu;
+using System;
 
 namespace Nop.Plugin.Widgets.Employees
 {
     public class EmployeesPlugin : BasePlugin, IWidgetPlugin, IAdminMenuPlugin
     {
-        private static bool resourcesCreated = false;
 #if NOP_PRE_4_3
         private readonly EmployeesObjectContext _objectContext;
 #endif
         private readonly ISettingService _settingService;
         private readonly IWebHelper _webHelper;
+        private readonly ILocalizationService _localizationService;
+        private readonly ILanguageService _languageService;
 
         public bool HideInWidgetList => false;
 
         public EmployeesPlugin(
             IWebHelper webHelper,
+            ILocalizationService localizationService,
+            ILanguageService languageService,
 #if NOP_PRE_4_3
             EmployeesObjectContext objectContext,
 #endif
@@ -34,6 +38,8 @@ namespace Nop.Plugin.Widgets.Employees
             ISettingService settingService)
         {
             _webHelper = webHelper;
+            _localizationService = localizationService;
+            _languageService = languageService;
 #if NOP_PRE_4_3
             _objectContext = objectContext;
 #endif
@@ -62,69 +68,42 @@ namespace Nop.Plugin.Widgets.Employees
         /// </summary>
         public override string GetConfigurationPageUrl() => $"{_webHelper.GetStoreLocation()}Admin/Employees/Configure";
 
-        internal void CreateLocaleStrings()
+        private void CreateLocaleStrings()
         {
-            if (!resourcesCreated)
+            void Create(string resourceName, int languageId, string resourceValue)
             {
-                resourcesCreated = true;
-                DoCreateLocaleStrings();
-            }
-        }
-
-        private void DoCreateLocaleStrings()
-        {
-            var localizationService = EngineContext.Current.Resolve<ILocalizationService>();
-            var languageService = EngineContext.Current.Resolve<ILanguageService>();
-
-            var availableLanguages = (from lang in languageService.GetAllLanguages()
-                                      select lang
-                                     ).ToDictionary(x => x.LanguageCulture, y => y);
-
-            IEnumerable<(LocaleStringAttribute lsa, Language language)> AsLSA(object[] values)
-            {
-                foreach (var v in values)
+                var lsr = _localizationService.GetLocaleStringResourceByName(resourceName, languageId, false);
+                if (lsr == null)
                 {
-                    var lsa = v as LocaleStringAttribute;
-                    if (availableLanguages.ContainsKey(lsa.Culture))
+                    lsr = new LocaleStringResource
                     {
-                        yield return (lsa, availableLanguages[lsa.Culture]);
-                    }
+                        LanguageId = languageId,
+                        ResourceName = resourceName,
+                        ResourceValue = resourceValue
+                    };
+                    _localizationService.InsertLocaleStringResource(lsr);
+                }
+                else if (lsr.ResourceValue != resourceValue)
+                {
+                    lsr.ResourceValue = resourceValue;
+                    _localizationService.UpdateLocaleStringResource(lsr);
                 }
             }
-            var resources = from type in GetType().Assembly.GetTypes()
-                            where type.CustomAttributes.Any(x => x.AttributeType == typeof(LocaleStringProviderAttribute))
-                            from field in type.GetFields()
-                            select (resourceName: field.GetValue(null).ToString(),
-                                    localeStrings: AsLSA(field.GetCustomAttributes(typeof(LocaleStringAttribute), false)));
 
-            foreach (var resource in resources)
+            ManageLocaleStrings(Create);
+        }
+
+        private void ManageLocaleStrings(Action<string, int, string> action)
+        {
+            foreach (var resource in PluginResources)
             {
                 foreach (var resourceLanguage in resource.localeStrings)
                 {
-                    void Create(string resourceName, string resourceValue)
-                    {
-                        var lsr = localizationService.GetLocaleStringResourceByName(resourceName, resourceLanguage.language.Id, false);
-                        if (lsr == null)
-                        {
-                            lsr = new LocaleStringResource
-                            {
-                                LanguageId = resourceLanguage.language.Id,
-                                ResourceName = resourceName,
-                                ResourceValue = resourceValue
-                            };
-                            localizationService.InsertLocaleStringResource(lsr);
-                        }
-                        else if (lsr.ResourceValue != resourceValue)
-                        {
-                            lsr.ResourceValue = resourceValue;
-                            localizationService.UpdateLocaleStringResource(lsr);
-                        }
-                    }
-                    Create(resource.resourceName, resourceLanguage.lsa.Value);
+                    action.Invoke(resource.resourceName, resourceLanguage.language.Id, resourceLanguage.lsa.Value);
 
                     if (!string.IsNullOrWhiteSpace(resourceLanguage.lsa.Hint))
                     {
-                        Create(resource.resourceName + ".Hint", resourceLanguage.lsa.Hint);
+                        action.Invoke(resource.resourceName + ".Hint", resourceLanguage.language.Id, resourceLanguage.lsa.Hint);
                     }
                 }
             }
@@ -158,8 +137,16 @@ namespace Nop.Plugin.Widgets.Employees
             _objectContext.Uninstall();
 #endif
 
-            //locales
-            //this.DeletePluginLocaleResource("Plugins.Widgets.Employees.Fields.Country");
+            void Delete(string resourceName, int languageId, string resourceValue)
+            {
+                var lsr = _localizationService.GetLocaleStringResourceByName(resourceName, languageId, false);
+                if (lsr != null)
+                {
+                    _localizationService.DeleteLocaleStringResource(lsr);
+                }
+            }
+
+            ManageLocaleStrings(Delete);
 
             base.Uninstall();
         }
@@ -170,5 +157,34 @@ namespace Nop.Plugin.Widgets.Employees
         {
             // TODO
         }
+
+        private IEnumerable<(string resourceName, IEnumerable<(LocaleStringAttribute lsa, Language language)> localeStrings)> PluginResources
+        {
+            get
+            {
+                var availableLanguages = (from lang in _languageService.GetAllLanguages()
+                                          select lang
+                                         ).ToDictionary(x => x.LanguageCulture, y => y);
+
+                IEnumerable<(LocaleStringAttribute lsa, Language language)> AsLSA(object[] values)
+                {
+                    foreach (var v in values)
+                    {
+                        var lsa = v as LocaleStringAttribute;
+                        if (availableLanguages.ContainsKey(lsa.Culture))
+                        {
+                            yield return (lsa, availableLanguages[lsa.Culture]);
+                        }
+                    }
+                }
+
+                return from type in GetType().Assembly.GetTypes()
+                       where type.CustomAttributes.Any(x => x.AttributeType == typeof(LocaleStringProviderAttribute))
+                       from field in type.GetFields()
+                       select (resourceName: field.GetValue(null).ToString(),
+                               localeStrings: AsLSA(field.GetCustomAttributes(typeof(LocaleStringAttribute), false)));
+            }
+        }
+
     }
 }
