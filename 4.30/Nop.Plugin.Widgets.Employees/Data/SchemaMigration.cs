4d8b392edfa8afcbbbbd92948039c9a542aa4fa7
Birgir Kristmannsson <birgir@centara.com>
2020-06-08 21:38:58 +0000

    4.30 added

---
+++ 4.30/Nop.Plugin.Widgets.Employees/Data/SchemaMigration.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using Nop.Data.Migrations;
+using Nop.Plugin.Widgets.Employees.Domain;
+
+namespace Nop.Plugin.Widgets.Employees.Data
+{
+    [SkipMigrationOnUpdate]
+    [NopMigration("2020/06/02 09:30:17:6455422", "Employees Plugin base schema")]
+    public class SchemaMigration : AutoReversingMigration
+    {
+        protected IMigrationManager _migrationManager;
+
+        public SchemaMigration(IMigrationManager migrationManager)
+        {
+            _migrationManager = migrationManager;
+        }
+
+        public override void Up()
+        {
+            _migrationManager.BuildTable<Department>(Create);
+            _migrationManager.BuildTable<Employee>(Create);
+        }
+    }
+}
---
+++ 4.30/Nop.Plugin.Widgets.Employees/Mapping/TableNameCompatibility.cs
@@ -0,0 +1,18 @@
+﻿using Nop.Data.Mapping;
+using Nop.Plugin.Widgets.Employees.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace Nop.Plugin.Widgets.Employees.Mapping
+{
+    public partial class TableNameCompatibility : INameCompatibility
+    {
+        public Dictionary<Type, string> TableNames => new Dictionary<Type, string>
+        {
+            { typeof(Department), "StatusDepartment" },
+            { typeof(Employee), "StatusEmployee" }
+        };
+
+        public Dictionary<(Type, string), string> ColumnName => new Dictionary<(Type, string), string>();
+    }
+}
