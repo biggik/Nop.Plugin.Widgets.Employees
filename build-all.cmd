0ff65bd22fe2d43a9cc1915964e941ce984e0d6b
Birgir Kristmannsson <birgir@centara.com>
2020-06-10 21:03:38 +0000

    Preparing for submitting plugin to nopCommerce

--- build-all.cmd
+++ build-all.cmd
@@ -1,12 +1,14 @@
 @echo off
-set sln_file=Nop.Plugin.Widgets.Employees
+set project=Nop.Plugin.Widgets.Employees
 
-cd 4.30
-dotnet build %sln_file% --configuration=Release
-dotnet build %sln_file% --configuration=Debug
+cd 4.30\%project%
+Echo building 4.30 of %project%
+rd ..\_build\release\. /s /q > 0
+dotnet build %project%.csproj --configuration=Release --no-incremental
 
-cd ..\4.20
-dotnet build %sln_file% --configuration=Release
-dotnet build %sln_file% --configuration=Debug
+cd ..\..\4.20\%project%
+Echo building 4.20 of %project%
+rd ..\_build\release\. /s /q > 0
+dotnet build %project%.csproj --configuration=Release --no-incremental
 
 cd ..
