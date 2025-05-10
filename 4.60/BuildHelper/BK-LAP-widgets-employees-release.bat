91ce14f8027df49d060d9310e322700c23f011c3
Birgir Kristmannsson <bk@mf.is>
2025-05-10 22:29:33 +0000

    4.60 removed

--- 4.60/BuildHelper/BK-LAP-widgets-employees-release.bat
+++
@@ -1,1 +0,0 @@
-call ..\..\BuildHelper\BuildHelper.bat 4.60 release %1 %2 %3 %4 %5
--- 4.60/BuildHelper/BuildHelper.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set helper=%cd%\..\BuildHelper\%computername%-widgets-employees-%1.bat
-Echo Running buildhelper for %1 on %computername% (%helper%)
-if exist %helper% ( call %helper% ) else ( echo Not found)
